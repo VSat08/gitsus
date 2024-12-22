474e36f03d5e7b2f1519533b10bb6f77424fb023
VSat08 <satyam2001verma@gmail.com>
2024-12-22 22:02:43 +0530

    Building Commit log component and integrating Gemini AI

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -4,19 +4,21 @@ import { useUser } from "@clerk/nextjs";
 import { Github, TrendingUp } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import CommitLog from "./commit-log";
 
 const DashboardPage = () => {
   const { project } = useProject();
   return (
     <div>
+      {/* {project?.id} */}
       <div className="flex flex-wrap items-center justify-between gap-y-4">
         {/* Github link connect  */}
         <div className="w-fit rounded-xl bg-primary px-4 py-3">
           <div className="flex items-center">
             <Github className="size-5 text-white" />
             <div className="ml-2">
-              <p className="text-sm font-medium text-white">
-                This project is linked to {' '}
+              <p className="text-xs font-medium text-white sm:text-sm">
+                This project is linked to{" "}
                 <Link
                   target="_blank"
                   href={project?.githubUrl ?? ""}
@@ -32,22 +34,17 @@ const DashboardPage = () => {
 
         <div className="h-4"></div>
 
-
         <div className="flex items-center gap-4">
-          TeamMembers
-          InviteButton
-          ArchiveButton
+          TeamMembers InviteButton ArchiveButton
         </div>
-
-
       </div>
-
       <div className="mt-4">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5  ">
-          AskQuestionCard
-          MeetingCard
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard MeetingCard
         </div>
       </div>
+      <div className="mt-8"></div>
+      <CommitLog />
     </div>
   );
 };
